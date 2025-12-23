9ca5688c87460b9c69a8ec99be20bbe4291331f8
kalinga890 <kalingaa69@gmail.com>
2025-12-23 17:10:44 +0530

    re-screen project setup

---
+++ src/main/java/com/rescreen/rescreen_service/RescreenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rescreen.rescreen_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescreenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescreenServiceApplication.class, args);
+	}
+
+}
